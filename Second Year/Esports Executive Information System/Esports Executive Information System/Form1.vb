e01b13873c0cd4591ad98e80a2879dc7c544e0dd
Sabriel Adriel San Agustin <sbrlzxc@gmail.com>
2025-10-14 14:26:08 +0800

    Delete Second Year/Esports Executive Information System directory

--- Second Year/Esports Executive Information System/Esports Executive Information System/Form1.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form1
-
-    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
-        Form2.Show()
-        Me.Close()
-
-    End Sub
-End Class
