57d6cc3fbcd8544060fcafe119e0f6d11ee955c1
Sabriel Adriel San Agustin <sbrlzxc@gmail.com>
2025-10-24 11:03:20 +0800

    Add files via upload

---
+++ Second Year/Esports Executive Information System/Esports Executive Information System/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        Form2.Show()
+        Me.Close()
+
+    End Sub
+
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+        Form3.Show()
+        Me.Close()
+
+    End Sub
+End Class
